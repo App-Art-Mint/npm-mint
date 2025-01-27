e152be4ca224548f4948c38a371e1d5a16660f85
Sam Underwood <sam@appartmint.com>
2025-01-26 20:29:51 -0500

    v2.1.10 - added MintWindow.height()

--- src/ts/imports/util/window.ts
+++ src/ts/imports/util/window.ts
@@ -2,7 +2,8 @@
  * Functions related to the browser window.
  */
 export abstract class MintWindow {
-	/**
+	
+    /**
      * Returns the width of the window, including fractional pixels
      * @returns the width of the window
      */
@@ -10,5 +11,13 @@ export abstract class MintWindow {
         const decimal: number = document.body.getBoundingClientRect().width % 1;
         return window.innerWidth + decimal;
     }
+
+    /**
+     * Returns the height of the window, including fractional pixels
+     * @returns the height of the window
+     */
+    static height () : number {
+        const decimal: number = document.body.getBoundingClientRect().height % 1;
+        return window.innerHeight + decimal;
+    }
 };
-export default MintWindow;
