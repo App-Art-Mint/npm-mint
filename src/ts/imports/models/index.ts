25af1dd996dde3179af9e0a5f641f19753edf53b
SunderApps <sam@appartmint.com>
2025-04-22 00:36:10 -0400

    v2.6.21 - card-grid image position top

--- src/ts/imports/models/index.ts
+++ src/ts/imports/models/index.ts
@@ -1,10 +1,8 @@
-/**
- * Forward all exports from the models directory
- */
-export * from './color';
-export * from './event';
-export * from './file';
-export * from './item';
-export * from './minify';
-export * from './page';
-export * from './recaptcha';
+export * from './color'
+export * from './event'
+export * from './file'
+export * from './item'
+export * from './minify'
+export * from './page'
+export * from './primitive'
+export * from './recaptcha'
---
+++ src/ts/imports/models/primitive.ts
@@ -0,0 +1,2 @@
+export type MintTPrimitive = string | number | boolean | undefined;
+export type MintTJson = Record<string, MintTPrimitive | MintTPrimitive[] | MintTJson[]>;
